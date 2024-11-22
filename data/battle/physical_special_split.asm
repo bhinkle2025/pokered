959b78d0f8daf3bbd684e58c0347c5a2555371a8
Brandon Hinkle <lewishinkle2020@gmail.com>
2024-11-22 03:40:45 -0500

    Added Physical/Special Split

---
+++ data/battle/physical_special_split.asm
@@ -0,0 +1,31 @@
+PhysicalToSpecialMoves:
+; Flying
+        db GUST
+; Poison
+        db ACID
+        db SLUDGE
+; Normal
+        db HYPER_BEAM
+        db SWIFT
+        db TRI_ATTACK
+        db -1 ; end
+
+SpecialToPhysicalMoves:
+; Fire
+        db FIRE_PUNCH
+; Water
+        db CLAMP
+        db CRABHAMMER
+        db WATERFALL
+; Grass
+        db RAZOR_LEAF
+        db VINE_WHIP
+; Electric
+        db THUNDERPUNCH
+; Ice
+        db ICE_PUNCH
+; Ghost
+        db LICK
+; Steel
+        db FLASH
+        db -1 ; end
