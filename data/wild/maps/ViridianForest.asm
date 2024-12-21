45b6c9b3e4cd1251ea32a5c8e74926ee06abb6f0
Brandon Hinkle <lewishinkle2020@gmail.com>
2024-12-21 14:18:02 -0500

    Biodiversity Update: Viridian Forrest

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -3,24 +3,24 @@ ViridianForestWildMons:
 IF DEF(_RED)
 	db  4, WEEDLE
 	db  5, KAKUNA
-	db  3, WEEDLE
-	db  5, WEEDLE
-	db  4, KAKUNA
-	db  6, KAKUNA
-	db  4, METAPOD
 	db  3, CATERPIE
+	db  5, PIDGEY
+	db  4, METAPOD
+	db  6, PIDGEOTTO
+	db  4, BEEDRILL
+	db  3, BUTTERFREE
 ENDC
 IF DEF(_BLUE)
 	db  4, CATERPIE
 	db  5, METAPOD
-	db  3, CATERPIE
-	db  5, CATERPIE
-	db  4, METAPOD
-	db  6, METAPOD
-	db  4, KAKUNA
 	db  3, WEEDLE
+	db  5, KAKUNA
+	db  4, PIDGEY
+	db  6, PIDGEOTTO
+	db  4, BEEDRILL
+	db  3, BUTTERFREE
 ENDC
-	db  3, PIKACHU
+	db  3, BULBASAUR
 	db  5, PIKACHU
 	end_grass_wildmons
 
