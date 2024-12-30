a312eb18efa365afff35bfb35999c781cbf1cf9a
Brandon Hinkle <lewishinkle2020@gmail.com>
2024-12-30 15:59:36 -0500

    Biodiversity Update: Route 8, Pokemon Tower, and Route 10

--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -3,13 +3,13 @@ PokemonTower6FWildMons:
 	db 21, GASTLY
 	db 22, GASTLY
 	db 23, GASTLY
-	db 24, GASTLY
-	db 20, GASTLY
-	db 19, GASTLY
 	db 26, HAUNTER
+	db 30, HAUNTER
+	db 28, HAUNTER
+	db 36, HAUNTER
 	db 22, CUBONE
 	db 24, CUBONE
-	db 28, HAUNTER
+	db 38, GENGAR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -2,24 +2,24 @@ Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, VOLTORB
 	db 16, SPEAROW
-	db 14, VOLTORB
+	db 14, MAGNEMITE
 IF DEF(_RED)
 	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
-	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
+	db 20, FEAROW
+	db 15, SANDSHREW
+	db 17, MACHOP
+	db 22, SANDSLASH
+	db 20, ARBOK
+	db 17, KRABBY
 ENDC
 IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 13, SPEAROW
+	db 11, EKANS
+	db 20, FEAROW
 	db 15, SANDSHREW
-	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, SANDSHREW
-	db 17, SANDSHREW
+	db 17, MACHOP
+	db 22, SANDSLASH
+	db 20, ARBOK
+	db 17, KRABBY
 ENDC
 	end_grass_wildmons
 
