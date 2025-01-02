acfd3e09cbe83f2ab5977055a9f3d623e12d835e
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-01-01 19:11:08 -0500

    Biodiversity Update: Power Plant

--- data/pokemon/base_stats/jolteon.asm
+++ data/pokemon/base_stats/jolteon.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/jolteon.pic", 0, 1 ; sprite dimensions
 	dw JolteonPicFront, JolteonPicBack
 
-	db TACKLE, SAND_ATTACK, QUICK_ATTACK, THUNDERSHOCK ; level 1 learnset
+	db DIG, SAND_ATTACK, QUICK_ATTACK, THUNDERBOLT ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
