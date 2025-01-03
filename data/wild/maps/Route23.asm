977dc9d35f1ae000e12abb97b6cbfae089468627
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-01-02 22:06:17 -0500

    Biodiversity Update: Route 21 & 23

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,25 +1,25 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 26, EKANS
+	db 38, ARBOK
 ENDC
 IF DEF(_BLUE)
-	db 26, SANDSHREW
+	db 38, ARBOK
 ENDC
 	db 33, DITTO
-	db 26, SPEAROW
-	db 38, FEAROW
-	db 38, DITTO
-	db 38, FEAROW
+	db 26, FEAROW
+	db 38, PRIMEAPE
+	db 38, EXEGGUTOR 
+	db 38, PERSIAN
 IF DEF(_RED)
-	db 41, ARBOK
+	db 38, SANDSLASH
 ENDC
 IF DEF(_BLUE)
-	db 41, SANDSLASH
+	db 38, SANDSLASH
 ENDC
-	db 43, DITTO
-	db 41, FEAROW
-	db 43, FEAROW
+	db 43, CHARIZARD
+	db 41, NIDOQUEEN
+	db 43, NIDOKING
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
