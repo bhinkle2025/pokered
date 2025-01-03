6ca415770827a3c5c6bf4c5b3fbd4da00c1bc450
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-01-02 21:21:20 -0500

    Biodiversity Update: Route 9

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -2,24 +2,24 @@ Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, RATTATA
 	db 16, SPEAROW
-	db 14, RATTATA
+	db 20, RATICATE
 IF DEF(_RED)
 	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
-	db 17, RATTATA
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
+	db 20, FEAROW
+	db 20, ARBOK
+	db 17, SANDSHREW
+	db 20, SANDSLASH
+	db 18, NIDORINA
+	db 18, NIDORINO
 ENDC
 IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 13, SPEAROW
-	db 15, SANDSHREW
-	db 17, RATTATA
-	db 17, SPEAROW
-	db 13, SANDSHREW
+	db 11, EKANS
+	db 20, FEAROW
+	db 20, ARBOK
 	db 17, SANDSHREW
+	db 20, SANDSLASH
+	db 18, NIDORINA
+	db 18, NIDORINO
 ENDC
 	end_grass_wildmons
 
