e8b407a1f36165e1067566cf9b2559b24b27bb9f
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-01-02 22:47:15 -0500

    Biodiversity Update: Route 24 & 25

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -1,26 +1,26 @@
 Route24WildMons:
 	def_grass_wildmons 25 ; encounter rate
 IF DEF(_RED)
-	db  7, WEEDLE
+	db  7, METAPOD
 	db  8, KAKUNA
-	db 12, PIDGEY
+	db 12, PIDGEOTTO
 	db 12, ODDISH
-	db 13, ODDISH
-	db 10, ABRA
-	db 14, ODDISH
-ENDC
-IF DEF(_BLUE)
-	db  7, CATERPIE
-	db  8, METAPOD
-	db 12, PIDGEY
-	db 12, BELLSPROUT
 	db 13, BELLSPROUT
 	db 10, ABRA
-	db 14, BELLSPROUT
+	db 16, KADABRA
 ENDC
-	db 13, PIDGEY
-	db  8, ABRA
-	db 12, ABRA
+IF DEF(_BLUE)
+	db  7, METAPOD
+	db  8, KAKUNA
+	db 12, PIDGEOTTO
+	db 12, ODDISH
+	db 13, BELLSPROUT
+	db 10, ABRA
+	db 16, KADABRA
+ENDC
+	db 13, VENONAT
+	db  8, PSYDUCK
+	db 12, SQUIRTLE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,28 +1,28 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
+	db  8, METAPOD
 	db  9, KAKUNA
-	db 13, PIDGEY
+	db 13, PIDGEOTTO
 	db 12, ODDISH
-	db 13, ODDISH
-	db 12, ABRA
-	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
-ENDC
-IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
-	db 13, PIDGEY
-	db 12, BELLSPROUT
 	db 13, BELLSPROUT
 	db 12, ABRA
-	db 14, BELLSPROUT
-	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
+	db 14, VENONAT
+	db 16, KADABRA
+	db  7, PSYDUCK
+	db  8, SQUIRTLE
+ENDC
+IF DEF(_BLUE)
+	db  8, METAPOD
+	db  9, KAKUNA
+	db 13, PIDGEOTTO
+	db 12, ODDISH
+	db 13, BELLSPROUT
+	db 12, ABRA
+	db 14, VENONAT
+	db 16, KADABRA
+	db  7, PSYDUCK
+	db  8, SQUIRTLE
 ENDC
 	end_grass_wildmons
 
