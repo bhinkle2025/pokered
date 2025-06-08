18897d3464f3618c16b3422b9036d1e8b7379af3
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-06-08 01:34:07 -0400

    Added Portable PC

--- engine/menus/draw_start_menu.asm
+++ engine/menus/draw_start_menu.asm
@@ -3,7 +3,7 @@ DrawStartMenu::
 	CheckEvent EVENT_GOT_POKEDEX
 ; menu with pokedex
 	hlcoord 10, 0
-	ld b, $0e
+	ld b, $10
 	ld c, $08
 	jr nz, .drawTextBoxBorder
 ; shorter menu if the player doesn't have the pokedex
@@ -33,7 +33,7 @@ DrawStartMenu::
 ; case for having pokedex
 	ld de, StartMenuPokedexText
 	call PrintStartMenuItem
-	ld a, $07
+	ld a, $08 ; edited for portablePC
 .storeMenuItemCount
 	ld [wMaxMenuItem], a ; number of menu items
 	ld de, StartMenuPokemonText
@@ -53,6 +53,11 @@ DrawStartMenu::
 	call PrintStartMenuItem
 	ld de, StartMenuOptionText
 	call PrintStartMenuItem
+	CheckEvent EVENT_GOT_POKEDEX ; new, for portablePC
+	jr z, .dontPrintPortablePC ; new, for portablePC
+	ld de, StartMenuPortablePCText ; new, for portablePC
+	call PrintStartMenuItem ; new, for portablePC
+.dontPrintPortablePC ; new, for portablePC
 	ld de, StartMenuExitText
 	call PlaceString
 	ld hl, wStatusFlags5
@@ -79,6 +84,9 @@ StartMenuExitText:
 
 StartMenuOptionText:
 	db "OPTION@"
+
+StartMenuPortablePCText: ; new
+	db "PORT.PC@"
 
 PrintStartMenuItem:
 	push hl
