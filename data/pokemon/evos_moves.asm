7c38a13e5943058a9400e6c2a98df87e4845225f
Brandon Hinkle <lewishinkle2020@gmail.com>
2026-09-05 02:09:44 -0400

    Trade Evo Fix

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -646,8 +646,8 @@ SlowpokeEvosMoves:
 
 KadabraEvosMoves:
 ; Evolutions
-	db EVOLVE_LEVEL, 42, ALAKAZAM
 	db EVOLVE_ITEM, LINKING_CABLE,1, ALAKAZAM
+	db EVOLVE_LEVEL, 42, ALAKAZAM
 	db 0
 ; Learnset
 	db 16, CONFUSION
@@ -660,8 +660,8 @@ KadabraEvosMoves:
 
 GravelerEvosMoves:
 ; Evolutions
-	db EVOLVE_LEVEL, 38, GOLEM
 	db EVOLVE_ITEM, LINKING_CABLE,1, GOLEM
+	db EVOLVE_LEVEL, 38, GOLEM
 	db 0
 ; Learnset
 	db 11, DEFENSE_CURL
@@ -686,8 +686,8 @@ ChanseyEvosMoves:
 
 MachokeEvosMoves:
 ; Evolutions
-	db EVOLVE_LEVEL, 38, MACHAMP
 	db EVOLVE_ITEM, LINKING_CABLE,1, MACHAMP
+	db EVOLVE_LEVEL, 38, MACHAMP
 	db 0
 ; Learnset
 	db 12, LEER
@@ -1776,8 +1776,8 @@ MissingNo92EvosMoves:
 
 HaunterEvosMoves:
 ; Evolutions
-	db EVOLVE_LEVEL, 42, GENGAR
 	db EVOLVE_ITEM, LINKING_CABLE,1, GENGAR
+	db EVOLVE_LEVEL, 42, GENGAR
 	db 0
 ; Learnset
 	db 19, POISON_GAS
